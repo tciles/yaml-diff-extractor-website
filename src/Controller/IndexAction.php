4f214ae6595220b5ffdfefc70604cb38d1ae3154
Thomas CILES <thomasciles@gmail.com>
2022-03-06 23:51:29 +0100

    fix(htaccess) add rewrite

--- src/Controller/IndexAction.php
+++ src/Controller/IndexAction.php
@@ -8,6 +8,7 @@ use App\Form\Type\FilesType;
 use App\Handler\YamlDiffExtractHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\File\Stream;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,10 +32,11 @@ use Throwable;
 class IndexAction extends AbstractController
 {
     public function __invoke(
-        Request $request,
-        TranslatorInterface $translator,
+        Request                $request,
+        TranslatorInterface    $translator,
         YamlDiffExtractHandler $handler
-    ): Response {
+    ): Response
+    {
         $form = $this->createForm(FilesType::class);
         $form->handleRequest($request);
 
@@ -45,15 +47,15 @@ class IndexAction extends AbstractController
             $fileB = $form->get('file_b')->getData();
 
             try {
-                $result = $handler->extract($fileA->getPathname(), $fileB->getPathname());
-
-                $response = new BinaryFileResponse($result);
-                $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT);
+                $file = $handler->extract($fileA->getPathname(), $fileB->getPathname());
+                $stream = new Stream($file->getPathname());
+                $response = new BinaryFileResponse($stream);
+                $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'diff.yaml');
                 $response->deleteFileAfterSend(true);
+
                 return $response;
             } catch (Throwable $e) {
                 $this->addFlash('error', $translator->trans('error.process'));
-                
                 return $this->redirectToRoute('app_index', [
                     '_locale' => $request->getLocale()
                 ]);
